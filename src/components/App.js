009c4d60e15523a77e010d99567db1fc42e37c9b
avishek2609 <123345471+avishek2609@users.noreply.github.com>
2023-03-26 15:38:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,40 +5,40 @@ import Pads from "./Pads";
 
 export const bank1 = {
   'Q': {
-    name: 'Heater 1',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    name: 'Heater 1'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
   },
   'W': {
     name: 'Heater 2',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
   },
   'E': {
     name: 'Heater 4',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
   },
   'A': {
     name: 'Heater 3',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
   },
   'S': {
     name: 'Clap',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
   },
   'D': {
     name: 'Open Hi-Hat',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
   },
   'Z': {
     name: 'Kick n Hat',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    source:"https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
   },
   'X': {
     name: 'Kick',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    source: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
   },
   'C': {
     name: 'Closed Hi-Hat',
-    source: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    source= "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
   }
 }
 
@@ -48,7 +48,7 @@ function ControlScreen() {
     <div id='control-screen'>
       <label id='label-power'>
         <input type='checkbox' id='power' />
-        <span className='checkmark'>Show Power of off here </span>
+        <span className='checkmark'>OFF or ON </span>
       </label>
       <label id='label-volume'>
         <input type='range'  id='volume' />
